803705607923df61361aefce767788a754c42b6b
kana315 <taguchi.kn@kanamekey.com>
2019-05-08 15:19:08 +0900

    作業中

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import React, { Component } from "react";
-// import logo from "./logo.svg";
-import CharacterList, { Character } from "./CharacterList";
+import * as React from "react";
+import "./App.css";
+import Description from "./Description";
 
-class App extends Component {
-  render() {
-    const characters: Character[] = [
-      {
-        id: 1,
-        name: "羽 咲 綾 乃 ",
-        age: 16,
-        height: 151
-      },
-      {
-        id: 2,
-        name: "荒 垣 な ぎ さ ",
-        age: 18,
-        height: 174
-      },
-      {
-        id: 3,
-        name: "泉 理 子 ",
-        age: 18
-      }
-    ];
+class App extends React.Component {
+  public render() {
     return (
-      <div className="container">
-        <header>
-          <h1>は ね バ ド ! キ ャ ラ ク タ ー 一 覧 </h1>
-        </header>
-        <CharacterList school="北 小 町 高 校 " characters={characters} />
+      <div className="App">
+        <Description countBy={1} />
       </div>
     );
   }
