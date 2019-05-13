afd632d2278fbd2c95c3dcaa73a2420547fb2315
RoyMcCrain <amber.amber0813@gmail.com>
2019-05-13 14:14:20 +0900

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import useCounter from "./useCounter";
+import Demo from "./Demo";
 
 const App: React.FC = () => {
+  const [count, dispatch] = useCounter({ type: "", value: 0 });
+  const textareaRef = useRef({} as HTMLTextAreaElement);
+  useLayoutEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.focus();
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. aaaaaa
-        </p>
+        <p>{count.value}</p>
+        <button onClick={() => dispatch({ ...count, type: "increase" })}>
+          Up
+        </button>
+        <button onClick={() => dispatch({ ...count, type: "degrease" })}>
+          down
+        </button>
+        <textarea ref={textareaRef} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +32,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <Demo initialCount={3} />
       </header>
     </div>
   );
